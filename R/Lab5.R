9a2b960f525c835ec67d7f42913b9f20c40544c2
Anna Koshyna <ann.koshina@gmail.com>
2018-12-01 23:03:18 +0200

    R Lab5 Task3 done

--- R/Lab5.R
+++ R/Lab5.R
@@ -7,7 +7,20 @@ print(pmean('specdata', 'sulfate', 55))
 print(pmean('specdata', 'nitrate'))
 
 #Task 2
+print(complete('specdata', 1))
+print(complete('specdata', c(2, 4, 8, 10, 12)))
+print(complete('specdata', 50:60))
 
-print(complete("specdata", 1))
-print(complete("specdata", c(2, 4, 8, 10, 12)))
-print(complete("specdata", 50:60))
+#Task 3
+cr <- corr('specdata', 150)
+print(head(cr)) 
+print(summary(cr))
+
+cr <- corr('specdata', 400)
+print(head(cr)) 
+print(summary(cr))
+
+cr <- corr('specdata', 5000)
+print(head(cr)) 
+print(summary(cr))
+print(length(cr))
--- R/pmean.R
+++ R/pmean.R
@@ -1,4 +1,4 @@
-pmean <- function(directory, pollutant, id=1:332) {
+read_files <- function(directory, id=1:332) {
   # locate the files
   # here we get all full files names that we need to read
   files <- list.files(directory, full.names=T)[id]
@@ -7,6 +7,12 @@ pmean <- function(directory, pollutant, id=1:332) {
   # and returns list of the results
   # so here we read all content of files in 'file' list into a 'data_list' list
   data_list <- lapply(files, read.csv)
+  
+  return (data_list)
+}
+
+pmean <- function(directory, pollutant, id=1:332) {
+  data_list <- read_files(directory, id)
 
   # 'data_list' list is the list that contains data.frames from all needed files
   # so here we merge list of data.frames into one big data.frame
@@ -18,18 +24,12 @@ pmean <- function(directory, pollutant, id=1:332) {
 }
 
 complete <- function(directory, id) {
-  # locate the files
-  # here we get all full files names that we need to read
-  files <- list.files(directory, full.names=T)[id]
-  
-  # function lapply(x, fun) execute function 'fun' over each element in list 'x'
-  # and returns list of the results
-  # so here we read all content of files in 'file' list into a 'data_list' list
-  data_list <- lapply(files, read.csv)
+  data_list <- read_files(directory, id)
 
   # so here we count number of complete cases for each list in data_list
-  # (one list per id) and then use unlist to make one vector of numbers
-  nobs <- unlist(lapply(data_list, function(x) { sum(complete.cases(x)) }))
+  # (one list per id) 
+  # sapply functions acts like lapply but returns a vector
+  nobs <- sapply(data_list, function(x) { sum(complete.cases(x)) })
 
   #create result as data frame with ids and number of complete cases
   result <- data.frame(id, nobs)
@@ -37,4 +37,24 @@ complete <- function(directory, id) {
   return (result)
 }
 
+corr <- function(directory, threshold = 0) {
+  data_list <- read_files(directory)
+  
+  # overwrite 'data_list' list of data.frames so data.frames(monitors) have only complete cases
+  data_list <- lapply(data_list, function(x) { x[complete.cases(x),] })
+  
+  # filter 'data_list' list so we have only list of data.frames where number of complete cases are more than threshold value 
+  # and in result we have 'filtered_list' list with needed monitors
+  filtered_list <- Filter(function(x) { nrow(x) > threshold }, data_list)
+  
+  # return empty vertor if filtered_list is empty 
+  # (if there are no monitors that have number of complete cases > threshold value)
+  if (length(filtered_list) == 0) {
+    return (c())
+  }
+  
+  # get vector of sulfate and nitrate correlation per each monitor
+  cors <- sapply(filtered_list, function(x) { cor(x[,'sulfate'], x[,'nitrate']) })
+  return (cors)
 
+}
