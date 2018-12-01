5cb3bd9079998ce4d4ecfb7bda86213e0f8edee9
Anna Koshyna <ann.koshina@gmail.com>
2018-12-01 21:34:15 +0200

    R Lab5 Task2 done

--- R/Lab5.R
+++ R/Lab5.R
@@ -1,6 +1,13 @@
+#use funtions from file pmean.R
 source('pmean.R')
 
 #Task 1
 print(pmean('specdata', 'sulfate', 1:10))
 print(pmean('specdata', 'sulfate', 55))
 print(pmean('specdata', 'nitrate'))
+
+#Task 2
+
+print(complete("specdata", 1))
+print(complete("specdata", c(2, 4, 8, 10, 12)))
+print(complete("specdata", 50:60))
--- R/pmean.R
+++ R/pmean.R
@@ -3,7 +3,7 @@ pmean <- function(directory, pollutant, id=1:332) {
   # here we get all full files names that we need to read
   files <- list.files(directory, full.names=T)[id]
   
-  # function lappy(x, fun) execute function 'fun' over each element in list 'x'
+  # function lapply(x, fun) execute function 'fun' over each element in list 'x'
   # and returns list of the results
   # so here we read all content of files in 'file' list into a 'data_list' list
   data_list <- lapply(files, read.csv)
@@ -16,3 +16,25 @@ pmean <- function(directory, pollutant, id=1:332) {
   result <- mean(data[,pollutant], na.rm = TRUE)
   return (result)
 }
+
+complete <- function(directory, id) {
+  # locate the files
+  # here we get all full files names that we need to read
+  files <- list.files(directory, full.names=T)[id]
+  
+  # function lapply(x, fun) execute function 'fun' over each element in list 'x'
+  # and returns list of the results
+  # so here we read all content of files in 'file' list into a 'data_list' list
+  data_list <- lapply(files, read.csv)
+
+  # so here we count number of complete cases for each list in data_list
+  # (one list per id) and then use unlist to make one vector of numbers
+  nobs <- unlist(lapply(data_list, function(x) { sum(complete.cases(x)) }))
+
+  #create result as data frame with ids and number of complete cases
+  result <- data.frame(id, nobs)
+  
+  return (result)
+}
+
+
