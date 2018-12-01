5cf7f0236549785657be7ded4431dc809333dddb
Anna Koshyna <ann.koshina@gmail.com>
2018-12-01 15:12:44 +0200

    R Lab2 Task2 done

--- R/Lab2.R
+++ R/Lab2.R
@@ -9,3 +9,15 @@ print(normals[10:20])
 print(normals[20:(20+10)])
 
 print(normals[normals > 0])
+
+#Task 2
+y <- data.frame(a = rnorm(100), b = 1:100, cc = sample(letters, 100, replace = TRUE))
+print(y)
+
+print(tail(y, 10))
+
+print(y[10:20,])
+
+print(y[10, 'b'])
+
+print(y[,'cc'])
